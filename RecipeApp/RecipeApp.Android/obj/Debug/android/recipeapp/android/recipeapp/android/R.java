0628135db50eddc44ebb882410ad308323b5813a
Jonathan Lyttle <myncijub@gmail.com>
2018-04-21 13:31:29 -0500

    Started layout

--- RecipeApp/RecipeApp.Android/obj/Debug/android/recipeapp/android/recipeapp/android/R.java
+++ RecipeApp/RecipeApp.Android/obj/Debug/android/recipeapp/android/recipeapp/android/R.java
@@ -10,6 +10,10 @@ package recipeapp.android.recipeapp.android;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int ZipCodeSearchLabel=0x7f040001;
+        public static final int relativeLayout1=0x7f040000;
+    }
     public static final class layout {
         public static final int main=0x7f020000;
     }
